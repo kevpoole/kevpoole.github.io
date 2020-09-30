ec7a1565fca3b2906d12030c2286447f4a9b15a3
kevpoole <71987733+kevpoole@users.noreply.github.com>
2020-09-30 01:38:22 -0400

    Update app.js

---
+++ animals/app.js
@@ -0,0 +1,14 @@
+var time = Math.random();
+
+var blup = document.querySelector('.blup');
+
+function setProperty(duration) {
+  blup.style.setProperty('--animation-time', duration +'s');
+}
+
+function changeAnimationTime() {
+  var animationDuration = Math.random();
+  setProperty(animationDuration);
+}
+
+setInterval(changeAnimationTime, 1000);
